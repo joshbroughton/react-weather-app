d57b69317cec1a080841506e01ab43211207241d
= <=>
2024-01-25 14:53:42 -0700

    add radio buttons for units

--- src/Weather.js
+++ src/Weather.js
@@ -3,10 +3,12 @@ import { useState } from "react";
 
 export default function Weather() {
   const [zipCode, setZipCode] = useState("");
+  const [units, setUnits] = useState("fahrenheit");
+
   return(
     <div>
       <h1>Weather</h1>
-      <WeatherForm zipCode={zipCode} setZipCode={setZipCode} />
+      <WeatherForm zipCode={zipCode} setZipCode={setZipCode} units={units} setUnits={setUnits} />
     </div>
   );
 }
--- src/WeatherForm.js
+++ src/WeatherForm.js
@@ -1,4 +1,4 @@
-export default function WeatherForm({ zipCode, setZipCode }) {
+export default function WeatherForm({ zipCode, setZipCode, units, setUnits }) {
   return(
   <form>
     <input
@@ -6,6 +6,30 @@ export default function WeatherForm({ zipCode, setZipCode }) {
       value={zipCode}
       onChange={(e) => setZipCode(e.target.value)}
       placeholder="enter a zip code" />
+    <label>
+      <input
+        type="radio"
+        checked={units === "fahrenheit"}
+        onClick={(e) => setUnits(e.target.value)}
+        value="fahrenheit"/>
+      Fahrenheit
+    </label>
+    <label>
+      <input
+        type="radio"
+        checked={units === "celsius"}
+        onClick={(e) => setUnits(e.target.value)}
+        value="celsius"/>
+      Celsius
+    </label>
+    <label>
+      <input
+        type="radio"
+        checked={units === "kelvin"}
+        onClick={(e) => setUnits(e.target.value)}
+        value="kelvin"/>
+      Kelvin
+    </label>
   </form>
   );
 };
